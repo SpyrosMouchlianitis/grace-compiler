b5817565152fc761e333f1d661c50c6c28918027
Spyros Mouchlianitis <spirosgsaaa@gmail.com>
2023-05-14 13:42:07 +0300

    Revert "Revert "wip""
    This reverts commit b8cd0d78e53391adc5b1e343cebbe820021269d5.
    
    # Conflicts:
    #	src/main/java/org/hua/dit/cup/parser.cup
    #	src/main/java/org/hua/dit/types/BasicType.java

---
+++ src/main/java/org/hua/dit/types/BasicType.java
@@ -0,0 +1,32 @@
+package org.hua.dit.types;
+
+public class BasicType extends Type {
+    
+    private enum BasicTypeEnum {
+        BOOL,
+        INT,
+        CHAR,
+        STRING
+    };
+
+    static public BasicType Int = new BasicType(BasicTypeEnum.INT);
+    static public BasicType Bool = new BasicType(BasicTypeEnum.BOOL);
+    static public BasicType Char = new BasicType(BasicTypeEnum.CHAR);
+    static public BasicType String = new BasicType(BasicTypeEnum.STRING);
+
+    private BasicTypeEnum type;
+
+    private BasicType(BasicTypeEnum e) {
+        type = e;
+    }
+
+    public String toString() { return type.toString(); }
+
+    public boolean equals(Type t) {
+        if (t instanceof BasicType) {
+            BasicType that = (BasicType)t;
+            return this.type == that.type;
+        }
+        return false;
+    }
+}
